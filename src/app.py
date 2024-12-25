dc19453f632b660091cb7b04f0b9a48c1178fc49
UswatunKhasanah209 <127113320+UswatunKhasanah209@users.noreply.github.com>
2024-12-25 13:56:57 +0700

    Delete src directory

--- src/app.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-
-pg = st.navigation([
-    st.Page("pages/home.py", title="HOME"),
-    st.Page("pages/classification.py", title="Klasifikasi Citra"),
-    st.Page("pages/dashboard.py", title="CNN/VGG"),
-    ])
-pg.run()
